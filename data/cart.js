028c589e1676bb98fc8b1c2c4251042764aadb48
Tejas-00 <tejassonawane1709@gmail.com>
2025-01-16 20:34:11 +0530

    deleting item from cart when clicked delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
     {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 2,
@@ -37,4 +37,16 @@ export function updateCartQuantity(productId) {
     const addedtocartElem = document.querySelector(`.js-added-to-cart-${productId}`);
     addedtocartElem.style.opacity = 1;
     const intervalId = setInterval(() => {addedtocartElem.style.opacity = 0;}, 1000);
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId != productId) {
+            newCart.push(cartItem);
+        }
+    })
+
+    cart = newCart;
 }
